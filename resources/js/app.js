e19654785d42192c1329418a11ac453bf2580ffd
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-07-26 14:31:18 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-import { Tooltip, Select, Modal, Ripple, Tab, Lightbox, initTWE, Carousel} from "tw-elements";
+import { Tooltip, Modal, Ripple, Tab, Lightbox, initTWE, Carousel} from "tw-elements";
 
 
 // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com
@@ -7,7 +7,6 @@ initTWE({ Tooltip });
 
 // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com
 // Initialization for ES Users
-initTWE({ Select });
 
 
 // Initialization for ES Users
