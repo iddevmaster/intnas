011d6462826ae2048e19a76a1402a7c260af0eaa
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-12-23 09:23:36 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <img src="/img/logoschool.jpg" alt="">
+                <img src="/img/logoschool.jpg" width="50%" alt="">
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
