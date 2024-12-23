b5f472718a5035b134e13ff2c7fe11b9fea3a993
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-12-23 09:25:56 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+            <div class="flex justify-center">
                 <img src="/img/logoschool.jpg" width="50%" alt="">
             </div>
 
